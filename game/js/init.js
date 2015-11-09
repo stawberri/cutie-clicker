38d4f35a496f5ba238b23c7ab1a7ea67e9407f3f
Aideen <aideen@aideen.pw>
2015-11-09 00:15:05 -0800

    Implement Rhaboo
    Make game launching more robust

--- game/js/init.js
+++ game/js/init.js
@@ -3,8 +3,47 @@ window.cc = window.cc ? cc : {};
 
 // Create init function
 cc.init = function() {
-  // Start game!
-  $('body').load('game/cutie.html');
+  // How many asynchronious things need to be completed?
+  var pendingActions = 1;
+
+  // This function starts the game once all the things are done
+  function startGame() {
+    // Decrement pendingActions and compare to 0
+    if(--pendingActions <= 0) {
+      $('body').load('game/cutie.html');
+    }
+  }
+
+
+  // Pre-launch tasks:
+
+
+  // Rhaboo (data storage library)
+  $.getScript('lib/rhaboo.min.js').done(function() {
+    // Create localStorage and sessionStorage data (respectively)
+    cc.ls = Rhaboo.persistent('cc-ls');
+    cc.ss = Rhaboo.perishable('cc-ss');
+
+    // Check localStorage version
+    switch(cc.ls.v) {
+      default:
+        cc.ls.write('d', {})
+
+        cc.ls.write('v', 1);
+      case 1:
+    }
+
+    // Check sessionStorage version
+    switch(cc.ss.v) {
+      default:
+        cc.ls.write('d', {})
+
+        cc.ls.write('v', 1);
+      case 1:
+    }
+
+    startGame();
+  });
 };
 
 // Run it to simplify calling code (for now)
