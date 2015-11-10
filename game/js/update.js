b27da6c911c46f6ffbeef9e9963cc074deb60142
Aideen <aideen@aideen.pw>
2015-11-09 18:38:42 -0800

    More loading messages

--- game/js/update.js
+++ game/js/update.js
@@ -3,23 +3,30 @@
   var dataStorageVersion = 1;
 
   // Load data
-  cc.ls = Rhaboo.persistent('cc-ls');
-  cc.ss = Rhaboo.perishable('cc-ss');
+  cc.init.addAction('&#9921;', function(done) { // ⛁
+    cc.ls = Rhaboo.persistent('cc-ls');
+    cc.ss = Rhaboo.perishable('cc-ss');
 
-  // Throw out sessionStorage if version doesn't match
-  if(cc.ss.v != dataStorageVersion) {
-      cc.ss.write('d', {})
-  }
+    done();
+  });
 
-  // Check localStorage version
-  switch(cc.ls.v) {
-    default: // there isn't a version
-      cc.ls.write('d', {});
-    break;
-    case dataStorageVersion:
-  }
+  // Data update processing
+  cc.init.addAction('&#9728;', function(done) { // ☀
+    // Throw out sessionStorage if version doesn't match
+    if(cc.ss.v != dataStorageVersion) {
+        cc.ss.write('d', {})
+    }
 
-  // Update versions
-  cc.ls.write('v', dataStorageVersion);
-  cc.ss.write('v', dataStorageVersion);
+    // Check localStorage version
+    switch(cc.ls.v) {
+      default: // there isn't a version
+        cc.ls.write('d', {});
+      break;
+      case dataStorageVersion:
+    }
+
+    // Update versions
+    cc.ls.write('v', dataStorageVersion);
+    cc.ss.write('v', dataStorageVersion);
+  });
 }();
