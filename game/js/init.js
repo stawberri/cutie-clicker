9c0513bbf17be15fde8bd62890b5834f847c705d
Aideen <aideen@aideen.pw>
2015-11-09 16:14:59 -0800

    Disallow multiple instances.

--- game/js/init.js
+++ game/js/init.js
@@ -7,7 +7,12 @@ window.cc = window.cc ? cc : {};
   cc.init = function() {
     // Keep track of pending tasks
     var pendingActions = [];
-    function addAction(action) {
+    function addAction(action, runFunction) {
+      // Create a default function that just returns argument
+      runFunction = runFunction || function(arg) {
+        return arg;
+      };
+
       var actionRemover = function() {
         // Remove this function from array
         pendingActions.splice(pendingActions.indexOf(actionRemover), 1);
@@ -24,49 +29,73 @@ window.cc = window.cc ? cc : {};
       // Set actionName to action parameter
       actionRemover.actionName = action;
 
-      return actionRemover;
+      return runFunction(actionRemover);
     }
 
-    // Create action to ensure this function has time to finsih running
-    var doneWithThisFunction = addAction('&#10003;'); // ✓
+    // This action ensures that all actions have time to start
+    addAction('&#10003;', function(done) { // ✓
 
-    // Pre-launch tasks:
+      // Existing instance checker
+      addAction('&#10063;', function(done) { // ❏
+        // Use this method of "checking" to see if localStorage works
+        try {
+          // Retrieve opposite of flag
+          var flag = window.localStorage.getItem('cc-instchk') || '0';
+          flag = flag == '0' ? '1' : '0';
+          // Update flag
+          window.localStorage.setItem('cc-instchk', flag);
 
+          // Watch flag for changes
+          $(window).on('storage', function(ev) {
+            console.log(ev.key);
+            if(ev.originalEvent.key == 'cc-instchk' && ev.originalEvent.storageArea == window.localStorage) {
+              // It changed! Exit!
+              window.location = 'about:blank';
+            }
+          });
 
+          done();
+        } catch(e) {
+          // Looks like it failed, and there's no need to check for multiple instances.
+          done();
+        }
+      });
 
-    // Rhaboo (data storage library)
-    var rhabooDone = addAction('&#9923;'); // ⛃
-    $.getScript('lib/rhaboo.min.js').done(function() {
-      // Create localStorage and sessionStorage data (respectively)
-      cc.ls = Rhaboo.persistent('cc-ls');
-      cc.ss = Rhaboo.perishable('cc-ss');
+      // Rhaboo (data storage library)
+      addAction('&#9923;', function(done) { // ⛃
+        $.getScript('lib/rhaboo.min.js').done(function() {
+          // Create localStorage and sessionStorage data (respectively)
+          cc.ls = Rhaboo.persistent('cc-ls');
+          cc.ss = Rhaboo.perishable('cc-ss');
 
-      // Check localStorage version
-      switch(cc.ls.v) {
-        default:
-          cc.ls.write('d', {})
+          // Check localStorage version
+          switch(cc.ls.v) {
+            default:
+              cc.ls.write('d', {})
 
-          cc.ls.write('v', 1);
-        case 1:
-      }
+              cc.ls.write('v', 1);
+            case 1:
+          }
 
-      // Check sessionStorage version
-      switch(cc.ss.v) {
-        default:
-          cc.ss.write('d', {})
+          // Check sessionStorage version
+          switch(cc.ss.v) {
+            default:
+              cc.ss.write('d', {})
 
-          cc.ss.write('v', 1);
-        case 1:
-      }
+              cc.ss.write('v', 1);
+            case 1:
+          }
 
-      rhabooDone();
+          done();
+        });
+      });
+
+      done();
+
     });
-
-    // All tasks complete
-    doneWithThisFunction();
   };
 
-  // Run it to simplify calling code (for now)
-  cc.init()
-
 }();
+
+// Run it to simplify calling code (for now)
+cc.init()
