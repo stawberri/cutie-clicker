bffc873b551f0a84b5bd44cd2a2cbcba281d6086
Aideen <aideen@aideen.pw>
2015-11-10 19:54:48 -0800

    No more duplicate mobile clicks

--- game/js/cutie.js
+++ game/js/cutie.js
@@ -5,27 +5,22 @@
 
   var surpriseTimeout;
 
-  var clickstart = function() {
+  clicker.on('mousedown', function() {
     clearTimeout(surpriseTimeout);
 
     cutie.addClass('clicked');
 
     // Update click counter and display
     $('#click-counter').html(cc.stats.clicks.add(1));
-  };
-  // Populate click counter
-  $('#click-counter').html(cc.stats.clicks());
+  });
 
-  var clickend = function() {
+  clicker.on('mouseup', function() {
     surpriseTimeout = setTimeout(function() {
       cutie.removeClass('clicked');
     }, 250);
-  };
+  });
 
-  clicker.on('touchstart', clickstart);
-  clicker.on('mousedown', clickstart);
-
-  clicker.on('touchend', clickend);
-  clicker.on('mouseup', clickend);
+  // Populate click counter
+  $('#click-counter').html(cc.stats.clicks());
 
 }();
