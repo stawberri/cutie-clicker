88e97be2f654d9f2fad66cbad208265100d61611
Aideen <aideen@aideen.pw>
2015-11-21 20:42:20 -0800

    Menu looks like a menu now

--- game/js/menu.js
+++ game/js/menu.js
@@ -30,4 +30,29 @@
     }
   });
 
+  // Forward clicks on middle of top bar to cutie card
+  $('#menu-close-button').click(function() {
+    if(data.active) {
+      // This doesn't prevent default, so be sure to check for the menu being active!
+      $('#cutie-bar-m').click();
+    }
+  }).on('mousedown touchstart', function(ev) {
+    ev.preventDefault();
+
+    // Fake a click on cutie-bar-m
+    $('#cutie-bar-m').addClass('fake-click');
+
+    // Have to detect if player releases mouse anywhere.
+    // Still doesn't work if they release it ouside of window
+    $('body').one('mouseup touchend', function(ev) {
+      ev.preventDefault();
+      $('#cutie-bar-m').removeClass('fake-click');
+
+      // But this doesn't click if player released mouse over cutie-bar-m, so fix that.
+      if($('#cutie-bar-m').is(ev.target)) {
+        $('#cutie-bar-m').click();
+      }
+    });
+  });
+
 }();
