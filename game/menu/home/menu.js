12087f6e0950b49dfe3bab25134e226aaf20dba9
Aideen <aideen@aideen.pw>
2015-12-06 19:30:02 -0800

    Https message

--- game/menu/home/menu.js
+++ game/menu/home/menu.js
@@ -4,6 +4,10 @@
 
   cc.menu[script] = function(element) {
     cc.util.getcss(dir + 'menu.css');
-    element.load(cc.util.l(dir + 'menu.html'));
+    element.load(cc.util.l(dir + 'menu.html'), function() {
+      if(location.protocol == 'https:') {
+        $('#menu-home-https-message').remove();
+      }
+    });
   };
 }();
