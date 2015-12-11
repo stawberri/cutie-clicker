790b88858ae71707115e89e59c791d1055d0600a
Aideen <aideen@aideen.pw>
2015-12-10 23:23:47 -0800

    Start working on showcases

---
+++ game/menu/showcase/menu.js
@@ -0,0 +1,15 @@
+!function() {
+  var script = 'showcase', dir = 'menu/' + script + '/',
+  menu = cc.menu[script] = function(element) {
+    cc.util.getcss(dir + 'menu.css');
+    element.load(cc.util.l(dir + 'menu.html'));
+  };
+
+  menu.open = function(open) {
+    if(open === false) {
+      setTimeout(function() {
+        cc.menu('home');
+      }, 300);
+    }
+  }
+}();
