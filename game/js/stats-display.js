c7afcd81204d7a63976ae24a317078f2da4a6eed
Aideen <aideen@aideen.pw>
2015-12-12 19:57:18 -0800

    Shop cooldown

--- game/js/stats-display.js
+++ game/js/stats-display.js
@@ -110,5 +110,40 @@
 
       $('html').addClass(burstClasses).removeClass(nonBurstClasses);
     });
-  })
+  });
+
+  // Time (countdown displays)
+  cc.loop.draw(function() {
+    $('.cv-countdown').html(function(index, oldHtml) {
+      var dataTime = $(this).attr('data-time');
+      if($.type(dataTime) === 'undefined') {
+        return;
+      }
+
+      // Calculate time
+      var time = Math.abs($.now() - dataTime);
+      var ms = time % 1000;
+      time = Math.floor(time / 1000);
+      var s = time % 60;
+      time = Math.floor(time / 60);
+      var m = time % 60;
+      time = Math.floor(time / 60);
+      var h = time;
+
+      // How much do we need to display?
+      if(h > 0) {
+        // Display hours, minutes (2 digits), and seconds (2 digits)
+        return h + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2);
+      } else if(m > 0) {
+        // Display minutes and seconds (two digits)
+        return m + ':' + ('0' + s).slice(-2);
+      } else if(s > 0) {
+        // Display seconds and ms
+        return (s + (ms / 1000)).toFixed(2);
+      } else {
+        // Just display ms
+        return ms;
+      }
+    });
+  });
 }();
--- game/menu/store/menu.js
+++ game/menu/store/menu.js
@@ -4,9 +4,17 @@
     cc.util.getcss(dir + 'menu.css');
     element.load(cc.util.l(dir + 'menu.html'), function() {
       $('#menu-store-buy-cutie').click(function(ev) {
+        // If cooldown is still running, don't do anything.
+        if(cc.util.rhanum(cc.ls.d, 'dailyShopCutie') > $.now()) {
+          return;
+        }
+
         if(cc.stats.mpcost('1000', true)) {
+          // Set cooldown to 23 hours
+          cc.util.rhanum(cc.ls.d, 'dailyShopCutie', $.now() + 82800000);
+
           // Disable button
-          $(this).prop('disabled', true)
+          $(this).prop('disabled', true);
 
           var index = cc.cuties.add('77');
           cc.effect.lightBurst({
@@ -17,7 +25,17 @@
             cc.menu('showcase', {type: 'cutie', index: index});
           });
         }
-      })
+      });
     });
+  };
+
+  menu.tick = function(now) {
+    // Disable button when it's on cooldown
+    if(cc.util.rhanum(cc.ls.d, 'dailyShopCutie') > $.now()) {
+      $('#menu-store-buy-cutie .countdown').addClass('cv-countdown').attr('data-time', cc.util.rhanum(cc.ls.d, 'dailyShopCutie'));
+      $('#menu-store-buy-cutie').prop('disabled', true);
+    } else {
+      $('#menu-store-buy-cutie').prop('disabled', false);
+    }
   };
 }();
