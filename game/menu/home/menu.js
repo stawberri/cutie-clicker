8990a837d964cae1255716665780497aee7c8741
Aideen <aideen@aideen.pw>
2015-12-20 00:56:00 -0800

    Share buttons

--- game/menu/home/menu.js
+++ game/menu/home/menu.js
@@ -6,6 +6,48 @@
       if(location.protocol == 'https:') {
         $('#menu-home-https-message').remove();
       }
+
+      function shareMessage(tag) {
+        var message = 'I have given ' + cc.stats.clicks() + ' pets to my ';
+        message += cc.util.rhanum(cc.ls.d, 'totalCuties') + ' ' + (tag ? '#CutieClicker' : 'Cutie Clicker') + ' cuties.';
+
+        var messageFragments = [
+          'Beat me.',
+          'Try and catch up.',
+          'You?',
+          'And you?',
+          'Join me.',
+          'It\'s fun.',
+          'Let\'s play.',
+          'You can too.',
+          'Click click.',
+          'Pet them.',
+          'Your turn.',
+          'Your move.',
+          'Fite me.'
+        ];
+
+        var randomMessage = messageFragments[Math.floor(Math.random() * messageFragments.length)];
+
+        return encodeURIComponent(message + ' ' + randomMessage);
+      }
+
+      $('#menu-home-clicks-tweet').click(function() {
+        var twitterURL = 'https://twitter.com/intent/tweet?text=';
+
+        window.open(twitterURL + shareMessage(true) + '%20cc.aideen.pw', '_blank');
+      });
+
+      $('#menu-home-clicks-tumblr').click(function() {
+        var tumblrUrl = 'https://www.tumblr.com/widgets/share/tool?posttype=link&tags=Cutie%20Clicker,CutieClicker&';
+        tumblrUrl += 'canonicalUrl=https%3A%2F%2Fcc.aideen.pw&content=https%3A%2F%2Fcc.aideen.pw&title=';
+
+        window.open(tumblrUrl + shareMessage(), '_blank');
+      });
+
+      $('#menu-home-clicks-copypaste').click(function() {
+        prompt('Please copy this manually.', decodeURIComponent(shareMessage()) + ' http://cc.aideen.pw');
+      });
     });
   };
 }();
